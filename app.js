477afdaecf05a1b76e5c44185d3bb5f4c0d0c5d7
Yemi Oduyemi <yhermii@gmail.com>
2023-11-21 11:23:56 +0100

    Port change

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use("/docs", eraseRouter);
 
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
