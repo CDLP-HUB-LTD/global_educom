5374cdb1cd252a5b8b63027583443977da807673
Yemi Oduyemi <yhermii@gmail.com>
2023-11-21 12:39:48 +0100

    update

--- routes/connect.js
+++ routes/connect.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 const apiEndpoint = 'https://globaleducomm.com';
 
-router.get('/api', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
     const response = await axios.get(`${apiEndpoint}/api`);
 
     const responseData = response.data;
 
-    res.json({ success: true, data: responseData });
+    res.json({ success: true, data: "Hello world!" });
   } catch (error) {
     console.error('Error connecting to API:', error.message);
     res.status(500).json({ success: false, error: error.message });
