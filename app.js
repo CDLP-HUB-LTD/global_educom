93c25ffdf166ac3e259662272b55d7ae230fcf83
Yemi Oduyemi <yhermii@gmail.com>
2023-12-12 13:00:59 +0100

    middleware

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const databaseMiddleware = require('./db');
 const fetchRouter = require('./routes/fetch');
 const sendRouter = require('./routes/send');
 const updateRouter = require('./routes/update');
