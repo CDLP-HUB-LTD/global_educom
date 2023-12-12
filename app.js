8d8962573cbe617f8f8d62f3e53e2e98d7f8061d
Yemi Oduyemi <yhermii@gmail.com>
2023-12-12 07:16:54 +0100

    update 5

--- app.js
+++ app.js
@@ -2,26 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
-const databaseMiddleware = require("./db");
+const databaseMiddleware = require('./db');
 const fetchRouter = require('./routes/fetch');
 const sendRouter = require('./routes/send');
-const updateRouter = require("./routes/update");
-const eraseRouter = require("./routes/erase");
+const updateRouter = require('./routes/update');
+const eraseRouter = require('./routes/erase');
 const connectRouter = require('./routes/connect');
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://globaleducom.vercel.app',
-  credentials: true,
-  allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
-  methods: 'GET, POST, PUT, DELETE',
-}));
-
-
-
-
-
+app.use(
+  cors({
+    origin: 'https://globaleducom.vercel.app',
+    credentials: true,
+  })
+);
 
 const options = {
   definition: {
@@ -32,36 +27,31 @@ const options = {
       description: 'The API for Global Educom',
     },
   },
-  apis: ['./routes/*.js'], 
+  apis: ['./routes/*.js'],
 };
+
+app.options('*', cors());
 
 app.use(databaseMiddleware);
 
 const specs = swaggerJsdoc(options);
 
-// Routes
-app.use('/', fetchRouter);
-app.use('/docs/send', cors(), sendRouter); 
-app.use('/docs/update', cors(), updateRouter);
-app.use('/docs/erase', cors(), eraseRouter);
-app.use('/connect', connectRouter);
-
-
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
-
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Origin', 'https://globaleducom.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 
+// Routes
+app.use('/', fetchRouter);
+app.use('/docs/send', cors(), sendRouter);
+app.use('/docs/update', cors(), updateRouter);
+app.use('/docs/erase', cors(), eraseRouter);
+app.use('/connect', connectRouter);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
