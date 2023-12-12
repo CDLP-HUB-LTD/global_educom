03edc3a0b6f602178365bc0b6244a77083131a50
Yemi Oduyemi <yhermii@gmail.com>
2023-12-12 10:52:52 +0100

    update3

--- routes/send.js
+++ routes/send.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const crypto = require("crypto");
 const validator = require('validator');
+const db = require('../db');
 
 
 // async function hashPassword(password) {
