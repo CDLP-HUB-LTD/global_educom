ea046d6b7af53af902dc4ae6bd113084d6175991
Yemi Oduyemi <yhermii@gmail.com>
2023-12-15 08:14:43 +0100

    johnny

--- routes/send.js
+++ routes/send.js
@@ -64,6 +64,8 @@ router.post('/register', async (req, res) => {
 
 
 router.post('/login', async (req, res) => {
+  johnny = "SELECT user_id, user_fname, user_email, user_password, salt FROM user WHERE user_email = 'johnnybravo@yahoo.com'";
+  console.log("User John = "+johnny);
   const { email, password } = req.body;
   if (!email || !password) {
     return res.status(400).json({ error: { message: 'All fields are required' } });
@@ -113,9 +115,6 @@ router.post('/login', async (req, res) => {
     const token = jwt.sign({ userId: user.user_id }, secretKey, { expiresIn: '1h' });
 
     req.session.user = sessionUser;
-
-    johnny = "SELECT user_id, user_fname, user_email, user_password, salt FROM user WHERE user_email = 'johnnybravo@yahoo.com'";
-    console.log("User John = "+johnny)
 
     return res.status(200).json({
       message: 'Login successful',
